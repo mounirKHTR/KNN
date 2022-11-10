ed20f8d140c0a3066f7e217231d485534a5740e1
Paul-louis Gomis <paullouis.gomis.etu@univ-lille.fr>
2022-11-10 16:58:26 +0100

    test distance valide(avant normalisation)

---
+++ test/testDistance.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class testDistance {
+	
+	DistanceEuclidienne dE;
+	DistanceManhattan dM;
+	List<IColumn> colI;
+	DatasetIris dtI;
+	
+	@BeforeEach
+	void init() {
+		dE = new DistanceEuclidienne() ;
+		dM = new DistanceManhattan();
+		colI = new ArrayList<IColumn>();
+		dtI = new DatasetIris(dE, colI);
+		dtI.loadFromFile("iris.csv");
+	}
+	
+	@Test
+	void testChargementIris() {
+		assertEquals(150,dtI.getNbLines());
+	}
+
+	@Test
+	void testDistanceEuclIris() {
+		assertEquals(Math.ceil(dE.distanceBetween(dtI,dtI.getSet().get(0),dtI.getSet().get(1))*1000)/1000,0.539);
+	}
+	
+	@Test
+	void testDistanceManhIris() {
+		
+		assertEquals(Math.ceil(dM.distanceBetween(dtI,dtI.getSet().get(0),dtI.getSet().get(1))*100)/100,0.7);
+	}
+
+}
