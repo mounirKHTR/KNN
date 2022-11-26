5e0e5cf18a4b1b86e982bd4a6172322bf2e41ad6
Mounir Khatri <mounir.khatri.etu@univ-lille.fr>
2022-11-26 18:06:40 +0100

    OK

--- src/model/MethodeKnn.java
+++ src/model/MethodeKnn.java
@@ -11,7 +11,7 @@ public class MethodeKnn {
         Map<Double, IPoint> resultat = new LinkedHashMap<>();
         Map<Double, IPoint> tampon = new HashMap<>();
 
-        for(IPoint points: listePoint){
+        for (IPoint points : listePoint) {
             tampon.put(distance.EuclidianDistanceBetween(pointDonne, points, listColumn), points);
         }
 
@@ -24,7 +24,7 @@ public class MethodeKnn {
         Map<Double, IPoint> resultat = new LinkedHashMap<>();
         Map<Double, IPoint> tampon = new HashMap<>();
 
-        for(IPoint points: listePoint){
+        for (IPoint points : listePoint) {
             tampon.put(distance.ManhattanDistanceBetween(pointDonne, points, listColumn), points);
         }
 
@@ -42,15 +42,38 @@ public class MethodeKnn {
         return resultat;
     }
 
-    public List<IPoint> getNearestNeigbhour(Map<Double, IPoint> listeNearestNeigbhour, int nearestNeigbhour) {
+    public List<IPoint> getNearestNeigbhour(Map<Double, IPoint> listNeigbhour, int nearestNeigbhour) {
         List<IPoint> resultat = new ArrayList<>();
 
-        LinkedList<IPoint> tampon = (LinkedList<IPoint>) listeNearestNeigbhour.values();
+        LinkedList<IPoint> tampon = new LinkedList<>(listNeigbhour.values());
 
-        for(int i = 0; i < nearestNeigbhour; ++i) {
+        for (int i = 0; i < nearestNeigbhour; ++i) {
             resultat.add(tampon.get(i));
         }
 
         return resultat;
+    }
+
+    public String mostvalue(List<IPoint> nearest) {
+        String rslt="";
+        Map<String, Integer> groupCount = new HashMap<String, Integer>();
+
+        List<String> tamp = new ArrayList<>();
+        for (IPoint i : nearest
+        ) {
+            tamp.add(i.getGroup());
+        }
+        for (String word : tamp) {
+            Integer count = groupCount.get(word);
+            groupCount.put(word, (count == null) ? 1 : count + 1);
+        }
+        int max=0;
+        for (String groupe: groupCount.keySet()
+             ) {
+                if(groupCount.get(groupe)>max){
+                    rslt=groupe;
+                    max=groupCount.get(groupe);
+                }
+        }return rslt;
     }
 }
